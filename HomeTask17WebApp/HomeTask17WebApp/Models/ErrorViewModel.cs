94155621d5ac294f7dba5d655f8960bbca7f7a15
nikmorror37 <nikitamironchuk37@gmail.com>
2023-04-04 16:59:34 +0300

    Initial commit

---
+++ HomeTask17WebApp/HomeTask17WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HomeTask17WebApp.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
